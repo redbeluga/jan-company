130c9be7fd28e3681258490539af4c8abe5fcc5e
redbeluga <72532566+redbeluga@users.noreply.github.com>
2023-01-31 13:38:27 -0800

    fixed recipe data

--- src/components/RecipeData.js
+++ src/components/RecipeData.js
@@ -35,26 +35,6 @@ export const recipesData = [
       moods:["sweet", "snack", "drinks", "indian", "quick"],
     }
   },
-  { 
-    name: " Grandma’s Chocolate Chip Cookie", 
-    ingredients: ["½ cup brown sugar", "½ cup milk", "¼ cup flour", "1 tsp vanilla extract", "2 eggs", "1 stick of butter", "choclate chips"], 
-    timeToPrep: "45 Minutes", 
-    instructions: `Preheat Your stove to 450 degrees. 
-    Get a bowl. Take the brown sugar and flour and miz it together. 
-    Get another bowl and crack open two eggs and whisk. Put the melted butter and vanilla extract inside the egg bowl. 
-    Mix the dry and the wet bowl together. 
-    Take a baking sheet and shape 15 cookies. Sprinkle chocolate chips on top. 
-    Place the tray in the oven for 45 minutes. 
-    Take it out and wait for 5 minutes. Enjoy!`, 
-    img:"https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/7/17/1/FN_Simple-Chocolate-Chip-Cookies_s4x3.jpg.rend.hgtvcom.616.462.suffix/1438794106265.jpeg", 
-    id: 3, 
-    Data: { 
-      totRating: 5, 
-      numberOfRatings: 1, 
-      moodsRating: new Map(), 
-      moods:["American", "snack", "drinks", "sweet"],
-    }
-  },
   {
     name: "Fancy Grilled Cheese",
     ingredients: ["¼ cup Pepper Jack Cheese" , "½ cup American Cheese", "Sourdough Bread 2 slices", "Butter", "Milk"],
@@ -70,65 +50,7 @@ export const recipesData = [
       totRating: 0,
       numberOfRatings: 0,
       moodsRating: new Map(),
-      moods:["American", "lunch", "savory"],
-    }
-  },
-  {
-    name: "Fancy Grilled Cheese",
-    ingredients: ["¼ cup Pepper Jack Cheese" , "½ cup American Cheese", "Sourdough Bread 2 slices", "Butter", "Milk"],
-    timeToPrep: "15 Minutes",
-    instructions: `Take your Butter and place it on the pan. Wait until the butter melts but make sure it does not caramelize. 
-    Place your two slices of sour dough bread on top of the butter and flip both size. The bread should be a light brown. 
-    Take your two cheeses and evenly distribute it on top of the bread. 
-    Place the other slice on top and wait until the cheese becomes golden brown. 
-    Take it out and enjoy! `,
-    img: "https://www.thechunkychef.com/wp-content/uploads/2016/08/Ultimate-Grilled-Cheese-2.jpg",
-    id: 9,
-    Data: {
-      totRating: 0,
-      numberOfRatings: 0,
-      moodsRating: new Map(),
-      moods:["American", "lunch", "savory"],
-    }
-  },
-  { 
-    name: " Grandma’s Chocolate Chip Cookie", 
-    ingredients: ["½ cup brown sugar", "½ cup milk", "¼ cup flour", "1 tsp vanilla extract", "2 eggs", "1 stick of butter", "choclate chips"], 
-    timeToPrep: "45 Minutes", 
-    instructions: `Preheat Your stove to 450 degrees. 
-    Get a bowl. Take the brown sugar and flour and miz it together. 
-    Get another bowl and crack open two eggs and whisk. Put the melted butter and vanilla extract inside the egg bowl. 
-    Mix the dry and the wet bowl together. 
-    Take a baking sheet and shape 15 cookies. Sprinkle chocolate chips on top. 
-    Place the tray in the oven for 45 minutes. 
-    Take it out and wait for 5 minutes. Enjoy!`, 
-    img:"https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/7/17/1/FN_Simple-Chocolate-Chip-Cookies_s4x3.jpg.rend.hgtvcom.616.462.suffix/1438794106265.jpeg", 
-    id: 5, 
-    Data: { 
-      totRating: 5, 
-      numberOfRatings: 1, 
-      moodsRating: new Map(), 
-      moods:["American", "snack", "drinks", "sweet"],
-    }
-  },
-  { 
-    name: " Grandma’s Chocolate Chip Cookie", 
-    ingredients: ["½ cup brown sugar", "½ cup milk", "¼ cup flour", "1 tsp vanilla extract", "2 eggs", "1 stick of butter", "choclate chips"], 
-    timeToPrep: "45 Minutes", 
-    instructions: `Preheat Your stove to 450 degrees. 
-    Get a bowl. Take the brown sugar and flour and miz it together. 
-    Get another bowl and crack open two eggs and whisk. Put the melted butter and vanilla extract inside the egg bowl. 
-    Mix the dry and the wet bowl together. 
-    Take a baking sheet and shape 15 cookies. Sprinkle chocolate chips on top. 
-    Place the tray in the oven for 45 minutes. 
-    Take it out and wait for 5 minutes. Enjoy!`, 
-    img:"https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/7/17/1/FN_Simple-Chocolate-Chip-Cookies_s4x3.jpg.rend.hgtvcom.616.462.suffix/1438794106265.jpeg", 
-    id: 6, 
-    Data: { 
-      totRating: 5, 
-      numberOfRatings: 1, 
-      moodsRating: new Map(), 
-      moods:["American", "snack", "drinks", "sweet"],
+      moods:["american", "lunch", "savory"],
     }
   },
   { 
@@ -148,7 +70,42 @@ export const recipesData = [
       totRating: 5, 
       numberOfRatings: 1, 
       moodsRating: new Map(), 
-      moods:["American", "snack", "drinks", "sweet"],
+      moods:["american", "snack", "drinks", "sweet"],
     }
   },
+  {
+    name: "Cheese Corndog",
+    ingredients: ["1 cup of whole milk", "2 blocks of cheese", "heavy cream", "¼ cup sugar", "2 cups of flour"],
+    timeToPrep: "2 Minutes",
+    instructions: `Bake in oven for 30 minutes before taking it out. 
+    Add crisps to the corndog and add the cheese after it has melted. 
+    Finally, mix everything together and wait for 20 minutes for it to cool. 
+    Take it out and enjoy! `,
+    img:"https://theodehlicious.com/korean-mozzarella-corn-dog/",
+    id: 2,
+    Data: {
+    totRating: 0,
+    numberOfRatings: 0,
+    moodsRating: new Map(), 
+    moods:["american", "korean", "snack", "lunch", "savory"],
+    }
+  },
+  { 
+    name: " Alfredo Pasta", 
+    ingredients: ["1/2 block of American cheese","½ cup milk", "¼ cup flour", "bag of pasta", "2 eggs", "1 stick of butter", "oil"], 
+    timeToPrep: "75 Minutes", 
+    instructions: `Preheat Your stove to 450 degrees. 
+    Get a bowl. Take ingredients and mix. 
+    Get another bowl and crack open two eggs and whisk. Put the melted butter and American cheese inside the egg bowl. 
+    Mix the dry and the wet bowl together. 
+    Take a baking sheet and pour into a new clean bowl. 
+    Take it out and wait for 5 minutes. Enjoy!`, 
+    img: "https://realhousemoms.com/one-pot-alfredo-pasta/", 
+    id: 2, 
+    Data: { 
+      totRating: 0, 
+      numberOfRatings: 0, 
+      moodsRating: new Map(), 
+      moods:["italian", "american", "lunch", "dinner", "savory"],
+    } },
 ]
